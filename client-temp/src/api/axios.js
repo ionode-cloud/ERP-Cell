0d057bbeac4504dfc5c13cb8b55830e22a302de9
Pupunfzx <beheraj542@gmail.com>
2026-03-02 09:42:07 +0530

    change port

--- client-temp/src/api/axios.js
+++ client-temp/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api', // Changed from https://erp-cell-nlh0.onrender.com/api
+    baseURL: 'https://erp-cell-nlh0.onrender.com/api', // Changed from https://erp-cell-nlh0.onrender.com/api
 });
 
 api.interceptors.request.use((config) => {
